3c09ea0d29a1bc12edfec0fb0f3f7e82a65c8e38
envygod-dev <mbsavage0930@gmail.com>
2024-11-03 16:00:06 +0800

    updated backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,9 +13,17 @@ app.use('/api', placeRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-// OOroo mongo db deer db neegeed url tawih
-mongoose.connect('mongodb://localhost:27017/places')
+const username = 'monkhbatbilguun191@gmail.com';
+const password = 'fuckyou';
+const dbName = 'places';
+
+const mongoURL = `mongodb://${username}:${password}@localhost:27017/${dbName}`;
+
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => {
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     })
-    .catch(err => console.error(err));
+    .catch(err => console.error('MongoDB connection error:', err));
